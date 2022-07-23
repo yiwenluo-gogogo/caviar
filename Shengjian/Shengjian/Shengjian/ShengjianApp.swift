dce04705a39a7611bb771455141e1ab9d016de54
Jinyang Tang <jit098@ucsd.edu>
2022-07-22 23:09:40 -0400

    Shengjian init commit

---
+++ Shengjian/Shengjian/Shengjian/ShengjianApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShengjianApp.swift
+//  Shengjian
+//
+//  Created by Sophia Tang on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ShengjianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
